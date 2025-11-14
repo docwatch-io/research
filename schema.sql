93646200bce2080e02fd35d58ed4312a08091024
docwatch-io <nate@docwatch.io>
2025-11-14 11:04:59 -0500

    1.0.3

--- schema.sql
+++ schema.sql
@@ -2,15 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict BzwZ8fzAGkZLZvpsrWGRe0vdpxe8vf6524ueI6QcH6gV6xBJUWAhvR6aox5pgGu
-
 -- Dumped from database version 17.6 (Ubuntu 17.6-2.pgdg24.04+1)
 -- Dumped by pg_dump version 17.6 (Ubuntu 17.6-2.pgdg24.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -18,6 +15,12 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- Name: warehouse; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA IF NOT EXISTS warehouse;
 
 SET default_tablespace = '';
 
